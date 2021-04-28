08b0a851448d270258558556b5a6b28503af91c7
yoouali <youssefoualifessuoy@gmail.com>
2021-04-28 13:58:02 +0000

    get photos from datat and create post

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,28 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-phtos';
+import Post from './post';
+
 export default function Timeline(){
-    return <div className="container col-span-2">
-        <p> TimeLine here! </p>
-    </div>;
+
+//we need to get the logged in user's phots
+
+const {photos} = usePhotos();
+console.log('photos', photos);
+//on loading the photos, we need to use react skeleton
+//if we have photos, render theme (create a post component)
+//if the user no photos, tell them to creat som photos
+
+return (<div className="container col-span-2">
+        {!photos ? (
+            <>
+            {[...new Array(4)].map((_, index) => (
+                <Skeleton key={index} count={1} width={640} height={600} className="mb-5"/>
+            ))}
+        </>
+      ) : photos?.length > 0 ? (
+          photos.map((content) => <Post key={content.docId} content={content} />)
+      ) : (
+          <p className="text-center text-2xl">Follow pepole to see there photos</p>
+      )}
+      </div>);
 }

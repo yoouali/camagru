1a0b45918434929cbe1b9981b6895a17c49f7342
yoouali <youssefoualifessuoy@gmail.com>
2021-04-29 17:09:51 +0000

    profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,37 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
+import Header from '../components/header';
+import { doesUsernameExist } from '../services/firebase';
+import UserProfile from '../components/profile';
+
 export default function Profile() {
-    return null;
+    const { username} = useParams();
+    const [userExists, setUserExists] = useState(false);
+    const [user, setUser] = useState(false);
+    const history = useHistory();
+
+    useEffect(() => {
+        async function checkUserExists() {
+            const user = await getUserByUsername(username);
+            if (user.length > 0) {
+                setUser(user[0]);
+                setUserExists(true);
+            } else {
+                setUserExists(false);
+                history.push(ROUTES.NOT_FOUND);
+            }
+        }
+        checkUserExists();
+    }, [username, history ]);
+
+    return userExists ? (
+        <div className="bg-blue-lighter">
+        <Header />
+        <div className="mx-auto max-w-screen-lg">
+          <UserProfile user={user} />
+        </div>
+      </div>
+    ) : null;
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -11,6 +11,19 @@ export async function doesUsernameExist(username) {
     return result.docs.map((user) => user.data().length > 0);
   }
 
+  export async function getUserByUsername(username) {
+    const result = await firebase
+      .firestore()
+      .collection('users')
+      .where('username', '==', username)
+      .get();
+  
+    return result.docs.map((item) => ({
+      ...item.data(),
+      docId: item.id
+    }));
+  }
+
 //get user from the firebase where userId === userId (passed from the auth)
 export async function getUserByUserId(userId) {
   const result = await firebase
@@ -94,4 +107,52 @@ export async function getPhotos(userId, following) {
   );
 
   return photosWithUserDetails;
+}
+
+export async function getUserPhotosByUserId(userId) {
+  const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', '==', userId)
+    .get();
+
+  const photos = result.docs.map((photo) => ({
+    ...photo.data(),
+    docId: photo.id
+  }));
+  return photos;
+}
+
+export async function isUserFollowingProfile(loggedInUserUsername, profileUserId) {
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('username', '==', loggedInUserUsername) // karl (active logged in user)
+    .where('following', 'array-contains', profileUserId)
+    .get();
+
+  const [response = {}] = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id
+  }));
+
+  return response.userId;
+}
+
+export async function toggleFollow(
+  isFollowingProfile,
+  activeUserDocId,
+  profileDocId,
+  profileUserId,
+  followingUserId
+) {
+  // 1st param: karl's doc id
+  // 2nd param: raphael's user id
+  // 3rd param: is the user following this profile? e.g. does karl follow raphael? (true/false)
+  await updateLoggedInUserFollowing(activeUserDocId, profileUserId, isFollowingProfile);
+
+  // 1st param: karl's user id
+  // 2nd param: raphael's doc id
+  // 3rd param: is the user following this profile? e.g. does karl follow raphael? (true/false)
+  await updateFollowedUserFollowers(profileDocId, followingUserId, isFollowingProfile);
 }
